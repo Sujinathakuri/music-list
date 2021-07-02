ce0c215d635d1d72e33cc53af514016670d4d138
Sujinathakuri <sujchand2@gmail.com>
2021-07-02 11:10:16 -0400

    save index.js basic content

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ const myUsers = {
 }
 
 const myCoolFunction = async() => {
-    const response = await fetch("https://openwhyd.org/adrien?format=json&fbclid=IwAR29DWuhfdgqGoxkMazwxry1URRMoB1ASQTm_boVcwJvckTuZmZcGUiRL4");
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const jsonUserData = await response.json();
     console.log(jsonUserData);
     return jsonUserData;
