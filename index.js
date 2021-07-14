01fe1056e950ca7e46511b15ade9a441471b6266
Sujinathakuri <sujchand2@gmail.com>
2021-07-13 21:55:13 -0400

    save index.js basic content

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ function fetchBooks() {
 
 function renderBooks(books) {
     const main = document.querySelector('main');
-    books.forEach(book => {
+    book.forEach(books => {
         const h2 = document.createElement('h2');
         h2.innerHTML = book.name;
         main.appendChild(h2);
