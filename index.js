f42d71350c654611f85589f0c2c8b57efc571264
Sujinathakuri <sujchand2@gmail.com>
2021-07-13 21:58:18 -0400

    save index.js basic content

--- index.js
+++ index.js
@@ -106,25 +106,32 @@ var music = [];
 
 //Async JS
 
-document.addEventListener('DOMContentLoaded', function() {
-    fetchBooks();
-});
-
 function fetchBooks() {
-    return fetch('https://www.theaudiodb.com/api/v1/json/1/search.php?s=coldplay')
-        .then(resp => resp.json())
-        .then(data => {
-            renderBooks(data);
-        });
-}
+    // To pass the tests, don't forget to return your fetch!
 
-//fetchBooks();
+    // To pass the tests, don't forget to return your fetch!
+
+    return fetch("https://anapioficeandfire.com/api/books")
+        .then(response => response.json())
+        .then(json => renderBooks(json))
+
+}
 
 function renderBooks(books) {
     const main = document.querySelector('main');
-    book.forEach(books => {
+    books.forEach(book => {
+        const h2 = document.createElement('h2');
+        h2.innerHTML = book.name;
+        main.appendChild(h2);
+    });
+    const main = document.querySelector('main');
+    books.forEach(book => {
         const h2 = document.createElement('h2');
         h2.innerHTML = book.name;
         main.appendChild(h2);
     });
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetchBooks();
+});
