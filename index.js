701bc077962629d487249832ab5acc41d2c6712d
Sujinathakuri <sujchand2@gmail.com>
2021-07-15 10:11:50 -0400

    save index.js basic content

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ function fetchMusics() {
 
 function renderMusics(musics) {
     const main = document.querySelector('main');
-    Musics.forEach(Music => {
+    Music.forEach(Music => {
         const h2 = document.createElement('h2');
         h2.innerHTML = music.name;
         main.appendChild(h2);
